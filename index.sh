c912c062e5d21ab2fdb540f60d237bf5ffb9719c
fwqaaq <fwqaaq@gmail.com>
2024-07-21 23:45:21 +0800

    Update sed command

--- index.sh
+++ index.sh
@@ -3,5 +3,5 @@
 # 使用 sed 进行转换
 sed -E '
 1,3s/^[[:space:]]*#/!/
-4,$ s/^([^[:space:]]+).*$/|| \1^/
+4,$ s/^([^[:space:]]+).*$/||\1^/
 ' ./content-farm-terminator/src/blocklist/scam-sites.txt >scam-sites.txt
